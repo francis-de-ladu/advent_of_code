ef3bb7e409409952731cb4eea0bf946a603e0e36
Francis de Ladurantaye <fdl9044@gmail.com>
2020-12-11 18:38:40 -0500

    added day 10 solution

---
+++ 2020-12-10/solution.py
@@ -0,0 +1,32 @@
+import re
+
+import numpy as np
+
+# load and preprocess input puzzle
+
+with open("puzzle.txt", 'r') as input_file:
+    puzzle = input_file.read().splitlines()
+
+puzzle = [0] + sorted(map(int, puzzle))
+puzzle.append(puzzle[-1] + 3)
+
+puzzle = np.asarray(puzzle)
+
+
+# part 1
+
+diffs = puzzle[1:] - puzzle[:-1]
+unique, counts = np.unique(diffs, return_counts=True)
+diff_counts = dict(zip(unique, counts))
+
+print(diff_counts[1] * diff_counts[3])
+
+
+# part 2
+
+diffs_str = ''.join(map(str, diffs.tolist()))
+diffs = re.split(r"(?:3+)", diffs_str)
+
+arrangments = 1
+for group in diffs:
+    arrangments *= sum(range(1, len(group))) + 1
